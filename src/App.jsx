91b4186390f43375b5706b95be69f900f79004b2
Syed Abdullah <syedabdullah1722@gmail.com>
2025-08-31 01:51:56 +0500

    Refactor: created departments map

--- src/App.jsx
+++ src/App.jsx
@@ -16,16 +16,7 @@ const App = () => {
         window.scrollTo(0, 0);
     }, [currentPage]); // This runs whenever currentPage changes
 
-    const departments = [
-        { name: 'Corporate', description: 'Manages external relations and partnerships.' },
-        { name: 'Finance', description: 'Handles all financial aspects and fundraising.' },
-        { name: 'Logistics', description: 'Ensures smooth operation and resource management for events.' },
-        { name: 'Operations', description: 'Oversees the execution of all society activities.' },
-        { name: 'Guest Relations', description: 'Manages interactions with guests and speakers.' },
-        { name: 'Security', 'description': 'Ensures safety and security during events.' },
-        { name: 'Marketing', description: 'Promotes the society and its initiatives.' },
-        { name: 'Creatives', description: 'Designs and develops visual content and branding.' },
-    ];
+
 
     const faqs = [
         {
