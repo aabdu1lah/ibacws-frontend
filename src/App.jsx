85f69f80a43c25c3610dc674c4230e9ca01c1de2
Syed Abdullah <syedabdullah1722@gmail.com>
2025-08-31 01:54:40 +0500

    Refactor: created faqs map

--- src/App.jsx
+++ src/App.jsx
@@ -16,51 +16,6 @@ const App = () => {
         window.scrollTo(0, 0);
     }, [currentPage]); // This runs whenever currentPage changes
 
-
-
-    const faqs = [
-        {
-            question: "What is the Executive Council?",
-            answer: "The Executive Council (EC) is a team of dedicated students who lead and manage all initiatives of the Community Welfare Society. They are responsible for planning, organizing, and executing events that align with our goals."
-        },
-        {
-            question: "Who is eligible to apply?",
-            answer: "All currently enrolled university students with a passion for community service and leadership are encouraged to apply, however according to the IBA guidelines, only Sophomore and Junior year students are eligible for the Executive Council positions!"
-        },
-        {
-            question: "What is the application process?",
-            answer: "The application process typically involves submitting an online form, followed by an interview round. Successful candidates will be selected based on their skills, experience, and alignment with our society's vision."
-        },
-        {
-            question: "What are the benefits of joining the EC?",
-            answer: "Joining the EC offers a unique opportunity to develop leadership skills, gain practical experience in event management, network with like-minded individuals, and make a tangible positive impact on the community. It's also a great addition to your resume!"
-        },
-        {
-            question: "What is the time commitment?",
-            answer: "The time commitment varies depending on the department and the ongoing projects. EC members are expected to dedicate a few hours per week, with increased involvement during major events. We strive to be flexible and understand academic commitments."
-        },
-        {
-            question: "What is the role of a Management Team?",
-            answer: "Management Teams assist the Executive Council in their assigned departments with carrying out tasks and gaining hands-on experience while supporting society activities."
-        },
-        {
-            question: "Who can apply for the MT?",
-            answer: "All students of IBA who are interested in community welfare, volunteering, and learning about society work can apply."
-        },
-        {
-            question: "What are the benefits of being in the MT?",
-            answer: "You will contribute to meaningful welfare projects and sharpen your leadership/teamwork skills while building lasting connections within the IBA community."
-        },
-        {
-            question: "Is prior experience required to join the MT?",
-            answer: "No, MTs are designed for students with little or no prior experience. The goal is to help you learn and grow."
-        },
-        {
-            question: "Do we get to choose our department?",
-            answer: "Yes, applicants can mention their preferred department. Final placement depends on interest, skills, and the needs of CWS."
-        },
-    ];
-
     const toggleFaq = (index) => {
         setShowFaq(showFaq === index ? null : index);
     };
---
+++ src/constants/faqs.js
@@ -0,0 +1,56 @@
+import departments from "./departments.js";
+
+const faqs = [
+    {
+        question: "What is the Executive Council?",
+        answer: "The Executive Council (EC) is a team of dedicated students who lead and manage all initiatives of the Community Welfare Society. They are responsible for planning, organizing, and executing events that align with our goals.",
+        isMT: false
+    },
+    {
+        question: "Who is eligible to apply?",
+        answer: "All currently enrolled university students with a passion for community service and leadership are encouraged to apply, however according to the IBA guidelines, only Sophomore and Junior year students are eligible for the Executive Council positions!",
+        isMT: false
+    },
+    {
+        question: "What is the application process?",
+        answer: "The application process typically involves submitting an online form, followed by an interview round. Successful candidates will be selected based on their skills, experience, and alignment with our society's vision.",
+        isMT: false
+    },
+    {
+        question: "What are the benefits of joining the EC?",
+        answer: "Joining the EC offers a unique opportunity to develop leadership skills, gain practical experience in event management, network with like-minded individuals, and make a tangible positive impact on the community. It's also a great addition to your resume!",
+        isMT: false
+    },
+    {
+        question: "What is the time commitment?",
+        answer: "The time commitment varies depending on the department and the ongoing projects. EC members are expected to dedicate a few hours per week, with increased involvement during major events. We strive to be flexible and understand academic commitments.",
+        isMT: false
+    },
+    {
+        question: "What is the role of a Management Team?",
+        answer: "Management Teams assist the Executive Council in their assigned departments with carrying out tasks and gaining hands-on experience while supporting society activities.",
+        isMT: true
+    },
+    {
+        question: "Who can apply for the MT?",
+        answer: "All students of IBA who are interested in community welfare, volunteering, and learning about society work can apply.",
+        isMT: true
+    },
+    {
+        question: "What are the benefits of being in the MT?",
+        answer: "You will contribute to meaningful welfare projects and sharpen your leadership/teamwork skills while building lasting connections within the IBA community.",
+        isMT: true
+    },
+    {
+        question: "Is prior experience required to join the MT?",
+        answer: "No, MTs are designed for students with little or no prior experience. The goal is to help you learn and grow.",
+        isMT: true
+    },
+    {
+        question: "Do we get to choose our department?",
+        answer: "Yes, applicants can mention their preferred department. Final placement depends on interest, skills, and the needs of CWS.",
+        isMT: true
+    },
+];
+
+export default faqs;
