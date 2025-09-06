bb02f4acfed9e5b71f1037324566fb1a3b1e37a5
Syed Abdullah <syedabdullah1722@gmail.com>
2025-09-07 00:12:18 +0500

    updated mt deadline to 9th

--- src/constants/deadlines.js
+++ src/constants/deadlines.js
@@ -1,6 +1,6 @@
 const deadlines = {
     ec: { deadline: 'September 2, 2025', interviews: 'September 1 - September 2, 2025', announcement: 'September 4, 2025' },
-    mt: { deadline: 'September 7, 2025', interviews: 'TBD', announcement: 'TBD' }
+    mt: { deadline: 'September 9, 2025', interviews: 'TBD', announcement: 'TBD' }
 };
 
 export default deadlines;
